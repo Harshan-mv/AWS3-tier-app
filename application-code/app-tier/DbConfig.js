0b4bff08ab61cb40b972379d1dd7e9f5d04848ea
Harshan-mv <hv754310@gmail.com>
2026-07-06 12:20:44 +0530

    2nd

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
     DB_HOST : 'database-1.c1y0qs2qi794.ap-south-1.rds.amazonaws.com',
     DB_USER : 'admin',
-    DB_PWD : 'kastro2025',
+    DB_PWD : 'Kastro2026',
     DB_DATABASE : 'mysql'
 });
