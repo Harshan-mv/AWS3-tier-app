7897f9daf1780ba17a54b3f90b37cdfa1b81c4a1
Harshan-mv <hv754310@gmail.com>
2026-07-06 11:57:03 +0530

    first commit

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.c1y0qs2qi794.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'kastro2025',
+    DB_DATABASE : 'mysql'
+});
